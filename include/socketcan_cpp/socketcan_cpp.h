0cfc654bc3229954364fe47495f6366a062472a0
Csaba Sipos <siposcsaba89@gmail.com>
2017-12-10 15:57:07 +0100

    - source added

---
+++ include/socketcan_cpp/socketcan_cpp.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <string>
+#include <socketcan_cpp/socketcan_cpp_export.h>
+
+#ifndef HAVE_SOCKETCAN_HEADERS
+#define CAN_MTU 0
+#define CANFD_MTU 1
+#endif
+
+namespace scpp
+{
+
+    enum SocketMode
+    {
+        MODE_CAN_MTU = CAN_MTU,
+        MODE_CANFD_MTU = CANFD_MTU
+    };
+
+    struct CanFrame
+    {
+        uint32_t id = 0;
+        uint8_t len = 0;
+        uint8_t flags = 0;
+        uint8_t data[64];
+
+    };
+
+    enum SocketCanStatus
+    {
+        STATUS_OK = 1 << 0,
+        STATUS_SOCKET_CREATE_ERROR = 1 << 2,
+        STATUS_INTERFACE_NAME_TO_IDX_ERROR = 1 << 3,
+        STATUS_MTU_ERROR = 1 << 4, /// maximum transfer unit
+        STATUS_CANFD_NOT_SUPPORTED = 1 << 5, /// Flexible data-rate is not supported on this interface
+        STATUS_ENABLE_FD_SUPPORT_ERROR = 1 << 6, /// Error on enabling fexible-data-rate support
+        STATUS_WRITE_ERROR = 1 << 7,
+        STATUS_READ_ERROR = 1 << 8,
+        STATUS_BIND_ERROR = 1 << 9,
+    };
+
+    class SocketCan
+    {
+    public:
+        SOCKETCAN_CPP_EXPORT SocketCan();
+        SOCKETCAN_CPP_EXPORT SocketCan(const SocketCan &) = delete;
+        SOCKETCAN_CPP_EXPORT SocketCan & operator=(const SocketCan &) = delete;
+        SOCKETCAN_CPP_EXPORT SocketCanStatus open(const std::string & can_interface, SocketMode mode = MODE_CAN_MTU);
+        SOCKETCAN_CPP_EXPORT SocketCanStatus write(const CanFrame & msg);
+        SOCKETCAN_CPP_EXPORT SocketCanStatus read(CanFrame & msg);
+        SOCKETCAN_CPP_EXPORT SocketCanStatus close();
+        SOCKETCAN_CPP_EXPORT const std::string & interfaceName() const;
+        SOCKETCAN_CPP_EXPORT ~SocketCan();
+    private:
+        int m_socket = -1;
+        std::string m_interface;
+        SocketMode m_socket_mode;
+    };
+}
---
+++ src/socketcan_cpp.cpp
@@ -0,0 +1,168 @@
+#include "../include/socketcan_cpp/socketcan_cpp.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef HAVE_SOCKETCAN_HEADERS
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+/* CAN DLC to real data length conversion helpers */
+
+static const unsigned char dlc2len[] = { 0, 1, 2, 3, 4, 5, 6, 7,
+8, 12, 16, 20, 24, 32, 48, 64 };
+
+/* get data length from can_dlc with sanitized can_dlc */
+unsigned char can_dlc2len(unsigned char can_dlc)
+{
+    return dlc2len[can_dlc & 0x0F];
+}
+
+#endif
+
+namespace scpp
+{
+    SocketCan::SocketCan()
+    {
+    }
+    SocketCanStatus SocketCan::open(const std::string & can_interface, SocketMode mode)
+    {
+        m_interface = can_interface;
+        m_socket_mode = mode;
+#ifdef HAVE_SOCKETCAN_HEADERS
+
+        /* open socket */
+        if ((m_socket = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) 
+        {
+            perror("socket");
+            return STATUS_SOCKET_CREATE_ERROR;
+        }
+        int mtu;
+        struct sockaddr_can addr;
+        struct ifreq ifr;
+
+        strncpy(ifr.ifr_name, can_interface.c_str(), IFNAMSIZ - 1);
+        ifr.ifr_name[IFNAMSIZ - 1] = '\0';
+        ifr.ifr_ifindex = if_nametoindex(ifr.ifr_name);
+        if (!ifr.ifr_ifindex) {
+            perror("if_nametoindex");
+            return STATUS_INTERFACE_NAME_TO_IDX_ERROR;
+        }
+
+        addr.can_family = AF_CAN;
+        addr.can_ifindex = ifr.ifr_ifindex;
+
+        if (mode == MODE_CANFD_MTU) 
+        {
+            /* check if the frame fits into the CAN netdevice */
+            if (ioctl(s, SIOCGIFMTU, &ifr) < 0) {
+                perror("SIOCGIFMTU");
+                return STATUS_MTU_ERROR;
+            }
+            mtu = ifr.ifr_mtu;
+
+            if (mtu != CANFD_MTU) {
+                return STATUS_CANFD_NOT_SUPPORTED;
+            }
+
+            /* interface is ok - try to switch the socket into CAN FD mode */
+            if (setsockopt(s, SOL_CAN_RAW, CAN_RAW_FD_FRAMES,
+                &enable_canfd, sizeof(enable_canfd))) 
+            {
+                
+                return STATUS_ENABLE_FD_SUPPORT_ERROR;
+            }
+
+        }
+
+        const int timestamping_flags = (SOF_TIMESTAMPING_SOFTWARE | \
+            SOF_TIMESTAMPING_RX_SOFTWARE | \
+            SOF_TIMESTAMPING_RAW_HARDWARE);
+
+        if (setsockopt(m_socket, SOL_SOCKET, SO_TIMESTAMPING,
+            &timestamping_flags, sizeof(timestamping_flags)) < 0) {
+            perror("setsockopt SO_TIMESTAMPING is not supported by your Linux kernel");
+        }
+
+        ///* disable default receive filter on this RAW socket */
+        ///* This is obsolete as we do not read from the socket at all, but for */
+        ///* this reason we can remove the receive list in the Kernel to save a */
+        ///* little (really a very little!) CPU usage.                          */
+        //setsockopt(s, SOL_CAN_RAW, CAN_RAW_FILTER, NULL, 0);
+
+        if (bind(m_sockets, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+            perror("bind");
+            return STATUS_BIND_ERROR;
+        }
+#else
+        printf("Your operating system does not support socket can!");
+#endif
+        return STATUS_OK;
+    }
+    SocketCanStatus SocketCan::write(const CanFrame & msg)
+    {
+#ifdef HAVE_SOCKETCAN_HEADERS
+        struct canfd_frame frame;
+        memset(&frame, 0, sizeof(frame)); /* init CAN FD frame, e.g. LEN = 0 */
+        //convert CanFrame to canfd_frame
+        frame.can_id = msg.id;
+        frame.len = msg.len;
+        frame.flags = msg.flags;
+        memcpy(frame.data, msg.data, msg.len);
+
+        if (m_socket_mode == MODE_CANFD_MTU)
+        {
+            /* ensure discrete CAN FD length values 0..8, 12, 16, 20, 24, 32, 64 */
+            frame.len = can_dlc2len(can_len2dlc(frame.len));
+        }
+        /* send frame */
+        if (write(m_socket, &frame, m_socket_mode) != m_socket_mode) {
+            perror("write");
+            return STATUS_WRITE_ERROR;
+        }
+#else
+        printf("Your operating system does not support socket can!");
+#endif
+        return STATUS_OK;
+    }
+    SocketCanStatus SocketCan::read(CanFrame & msg)
+    {
+#ifdef HAVE_SOCKETCAN_HEADERS
+        struct canfd_frame frame;
+
+        // Read in a CAN frame
+        auto num_bytes = ::read(m_socket, &frame, CANFD_MTU);
+        if (num_bytes != CAN_MTU || num_bytes != CANFD_MTU)
+        {
+            perror("Can read error");
+            return STATUS_READ_ERROR;
+        }
+
+        msg.id = frame.can_id;
+        msg.len = frame.len;
+        msg.flags = frame.flags;
+        memcpy(msg.data, frame.data, frame.len);
+#else
+        printf("Your operating system does not support socket can!");
+#endif
+        return STATUS_OK;
+    }
+    SocketCanStatus SocketCan::close()
+    {
+#ifdef HAVE_SOCKETCAN_HEADERS
+        close(m_socket);
+#endif
+        return STATUS_OK;
+    }
+    const std::string & SocketCan::interfaceName() const
+    {
+        return m_interface;
+    }
+    SocketCan::~SocketCan()
+    {
+        close();
+    }
+}
