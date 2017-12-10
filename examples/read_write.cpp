d5a5b44a13f1b8c4c7f9e5ce2203cb1946963b27
Csaba Sipos <siposcsaba89@gmail.com>
2017-12-10 20:58:53 +0100

    error handling on sample

--- examples/read_write.cpp
+++ examples/read_write.cpp
@@ -3,30 +3,35 @@
 #include <iostream>
 
 int main()
-{
+{   
     scpp::SocketCan sockat_can;
-    sockat_can.open("can0");
-    scpp::CanFrame fr;
-
-    auto sc_status = sockat_can.read(fr);
-    if (sc_status != scpp::STATUS_OK)
-        printf("something went wrong on socket read, error code : %d \n", int32_t(sc_status));
+    if (sockat_can.open("can0") == scpp::STATUS_OK)
+    {
+        scpp::CanFrame fr;
+        
+        auto sc_status = sockat_can.read(fr);
+        if (sc_status != scpp::STATUS_OK)
+            printf("something went wrong on socket read, error code : %d \n", int32_t(sc_status));
+        else
+            printf("len %d byte, id: %d, data: %02x %02x %02x %02x %02x %02x %02x %02x  \n", fr.len, fr.id, 
+                fr.data[0], fr.data[1], fr.data[2], fr.data[3],
+                fr.data[4], fr.data[5], fr.data[6], fr.data[7]);
+        
+        scpp::CanFrame cf_to_write;
+        
+        cf_to_write.id = 123;
+        cf_to_write.len = 8;
+        for (int i = 0; i < 8; ++i)
+            cf_to_write.data[i] = i;
+        auto write_sc_status = sockat_can.write(cf_to_write);
+        if (write_sc_status != scpp::STATUS_OK)
+            printf("something went wrong on socket write, error code : %d \n", int32_t(write_sc_status));
+        else
+            printf("Message was written to the socket \n");
+    }
     else
-        printf("len %d byte, id: %d, data: %02x %02x %02x %02x %02x %02x %02x %02x  \n", fr.len, fr.id, 
-            fr.data[0], fr.data[1], fr.data[2], fr.data[3],
-            fr.data[4], fr.data[5], fr.data[6], fr.data[7]);
-
-    scpp::CanFrame cf_to_write;
-   
-    cf_to_write.id = 123;
-    cf_to_write.len = 8;
-    for (int i = 0; i < 8; ++i)
-        cf_to_write.data[i] = i;
-    auto write_sc_status = sockat_can.write(cf_to_write);
-    if (write_sc_status != scpp::STATUS_OK)
-        printf("something went wrong on socket write, error code : %d \n", int32_t(write_sc_status));
-    else
-        printf("Message was written to the socket \n");
-
+    {
+        printf("Cannot open can socket!");
+    }
     return 0;
-}
+}   
